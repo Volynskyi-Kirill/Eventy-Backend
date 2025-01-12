1d46f29950f132e8a35388952a9702a1edcc6cb1
Volynskyi-Kirill <kirill108bol@gmail.com>
2025-01-12 23:17:41 +0100

    config: cors

--- eventy/src/main.ts
+++ eventy/src/main.ts
@@ -3,12 +3,20 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
+const CorsConfig = {
+  origin: '*',
+  methods: 'GET,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-
   const configService = app.get(ConfigService);
   const port = configService.get('APPLICATION_PORT');
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(CorsConfig);
 
   await app.listen(port);
   Logger.log(`Application is running on port ${port}`, 'Bootstrap');
