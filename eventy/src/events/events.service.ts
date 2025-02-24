c72185ed900b24cb32a72cee6653622fabfc571a
Volynskyi-Kirill <kirill108bol@gmail.com>
2025-02-24 14:28:12 +0100

    feat: createEvent sevice

--- eventy/src/events/events.service.ts
+++ eventy/src/events/events.service.ts
@@ -1,13 +1,50 @@
 import { Injectable } from '@nestjs/common';
-import { CreateEventDto } from './dto/create-event.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateEventDto } from './dto/create-event.dto';
 
 @Injectable()
 export class EventsService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  createEvent(createEventDto: CreateEventDto) {
-    // const { eventZones, socialMedia, dates, ...eventDto } = createEventDto;
-    // return this.prismaService.event.create(createEventDto);
+  async createEvent(createEventDto: CreateEventDto) {
+    const {
+      dates,
+      eventZones,
+      socialMedia,
+      categoryIds,
+      speakerIds,
+      ...eventData
+    } = createEventDto;
+
+    return await this.prismaService.event.create({
+      data: {
+        ...eventData,
+        dates: {
+          create: dates.map((dateDto) => ({
+            date: new Date(dateDto.date),
+          })),
+        },
+
+        eventZones: {
+          create: eventZones,
+        },
+
+        socialMedia: {
+          create: socialMedia,
+        },
+
+        ...(categoryIds && {
+          categories: {
+            connect: categoryIds.map((id) => ({ id })),
+          },
+        }),
+
+        ...(speakerIds && {
+          speakers: {
+            connect: speakerIds.map((id) => ({ id })),
+          },
+        }),
+      },
+    });
   }
 }
